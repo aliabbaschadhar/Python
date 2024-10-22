db5458d45ad4731a748709811395c3d2472490f7
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-10-22 23:23:20 +0500

    add remaining solution code files

--- 02_conditionals/02_solution.py
+++ 02_conditionals/02_solution.py
@@ -1,10 +1,13 @@
 age = 22;
 day = "Wednesday"
 
+# Python ternary operator
+
 price = 12 if age>=18 else 8 
 
 if day == "Wednesday":
-    price = price-2;
+    # price = price-2;
+    price -= 2
     print("Price is: ",price)
 else:
     print("Price is: ", price);
